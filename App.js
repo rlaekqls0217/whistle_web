ab2c3c4a0304ba63917386aa0aa9f6998e2425d5
김다빈 <139124989+rlaekqls0217@users.noreply.github.com>
2023-09-22 13:07:14 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './component/Main';
+import About from './component/About';
+import Contact from './component/Contact';
+import Nav from './component/Nav';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Main />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
